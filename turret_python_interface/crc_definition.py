4e007e1aac340c2f3d1a7232aaf558d177aa2d2d
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-08-07 12:58:31 -0700

    Initial commit

---
+++ turret_python_interface/crc_definition.py
@@ -0,0 +1,10 @@
+from crc import Configuration, CrcCalculator
+
+crc_ethernet_config = Configuration(
+    width=32,
+    polynomial=0x4C11DB7,
+    init_value=0xFFFF_FFFF,
+    reverse_input=False,
+    reverse_output=False,
+    final_xor_value=0x0000_0000)
+crc_ethernet = CrcCalculator(crc_ethernet_config)
---
+++ turret_python_interface/request.py
@@ -0,0 +1,30 @@
+import cattr
+import attr
+from cobs import cobs
+import json
+
+from .crc_definition import crc_ethernet
+
+from enum import Flag
+
+from io import BytesIO
+
+
+@attr.dataclass
+class Request:
+    """a request object"""
+
+    kind: int  # u32 BE
+
+    def __bytes__(self):
+        buf = BytesIO()
+        payload = json.dumps(cattr.unstructure(self)).encode()
+        buf.write(payload)
+
+        payload_crc = crc_ethernet.calculate_checksum(payload[:len(payload)//4*4])
+        buf.write(payload_crc.to_bytes(4, "big", signed=False))
+
+        # seek to the start of the BytesIO buffer
+        buf.seek(0)
+        # return the formed buffer object.
+        return cobs.encode(buf.read())
