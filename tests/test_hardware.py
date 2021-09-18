8463b5e8dd53aaf7d8e84ef3bc11110ccec8d5e1
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-09-18 16:02:19 -0700

    More explanation of what the test is doing

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -13,8 +13,12 @@ from loguru import logger
 
 @pytest.mark.hardware
 def test_request():
+    """ Verifies that a valid telemetry response is received from the actual hardware. """
     logger.debug("spawning interface...")
     target = pathlib.Path("/") / "dev" / "ttyUSB0"
     interface = Interface(timeout=1, serial_path=target)
     logger.debug("retrieving telemetry...")
-    response = interface.get_telemetry()
+    # fetch telemetry
+    interface.get_telemetry()
+    # if we got this far, we received a valid response from the firmware.
+    # therefore, the test passes. (no asserts required for this.)
